b4e1ffeba58e3bc020bceb3e0650d45273b2f230
Jure Zbontar <jure.zbontar@gmail.com>
2014-05-26 03:20:32 +0200

    save PNGs

--- main.lua
+++ main.lua
@@ -15,17 +15,25 @@ tau2 = 6
 
 cutorch.setDevice(1)
 
+function savePNG(fname, vol)
+   local pred = torch.CudaTensor(1, 1, height, width)
+   adcensus.spatial_argmin(vol, pred)
+   pred:add(-1):div(disp_max)
+   image.savePNG(fname, pred[{1,1}])
+end
+
 x0 = image.loadPNG('data/tsukuba0.png', 3, 'byte'):float():resize(1, 3, height, width):cuda()
 x1 = image.loadPNG('data/tsukuba1.png', 3, 'byte'):float():resize(1, 3, height, width):cuda()
-pred = torch.CudaTensor(1, 1, height, width)
 
 -- ad volume
 ad_vol = torch.CudaTensor(1, disp_max, height, width)
 adcensus.ad(x0, x1, ad_vol)
+savePNG('report/img/absdiff.png', ad_vol)
 
 -- census volume
 census_vol = torch.CudaTensor(1, disp_max, height, width)
 adcensus.census(x0, x1, census_vol)
+savePNG('report/img/census.png', census_vol)
 
 -- adcensus volume
 function rho(c, lambda)
@@ -35,6 +43,7 @@ rho(ad_vol, ad_lambda)
 rho(census_vol, census_lambda)
 adcensus_vol = torch.CudaTensor(1, disp_max, height, width)
 adcensus_vol:add(ad_vol, 1, census_vol)
+savePNG('report/img/adcensus.png', adcensus_vol)
 
 -- cbca
 x0_t = torch.CudaTensor(1, 3, width, height):copy(x0:transpose(3, 4))
@@ -47,9 +56,5 @@ output = torch.CudaTensor(1, disp_max, height, width)
 for i = 1,4 do
    adcensus.cbca(x0, x1, input, output, L1, L2, tau1, tau2)
    input:copy(output)
+   savePNG(('report/img/cbca%d.png'):format(i), adcensus_vol)
 end
-
--- plot
-adcensus.spatial_argmin(adcensus_vol, pred)
-pred:add(-1):div(disp_max)
-image.savePNG('foo.png', pred[{1,1}])
