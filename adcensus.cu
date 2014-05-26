13cb0b70eb627563e46ab12c4a290ec9043b1e0f
Jure Zbontar <jure.zbontar@gmail.com>
2014-05-26 03:00:05 +0200

    cbca

--- adcensus.cu
+++ adcensus.cu
@@ -37,7 +37,7 @@ __global__ void ad(float *x0, float *x1, float *output, int size, int size3, int
 		} else {
 			for (int i = 0; i < 3; i++) {
 				int ind = i * size23 + xy;
-				dist += fabs(x0[ind] - x1[ind - d]);
+				dist += fabsf(x0[ind] - x1[ind - d]);
 			}
 		}
 		output[id] = dist / 3;
@@ -148,10 +148,80 @@ int spatial_argmin(lua_State *L)
 	return 0;
 }
 
+#define CBCA_CONDITIONS(xx, yy, xxx, yyy) (\
+    0 <= (yy) && (yy) < size2 && \
+    0 <= (xx) && (xx) < size3 && \
+    fabsf(x0[(yy) * size3 + (xx)] - x0[(yyy) * size3 + (xxx)]) < tau1 && ((\
+		fabsf((xx) - x) + fabsf((yy) - y) < L1 && \
+		fabsf(x0[(yy) * size3 + (xx)] - x0[y * size3 + x]) < tau2 && \
+		fabsf(x1[(yy) * size3 + (xx) - d] - x1[y * size3 + x - d]) < tau2) || (\
+		fabsf((xx) - x) + fabsf((yy) - y) < L2 && \
+		fabsf(x0[(yy) * size3 + (xx)] - x0[y * size3 + x]) < tau1 && \
+		fabsf(x1[(yy) * size3 + (xx) - d] - x1[y * size3 + x - d]) < tau1)))
+
+__global__ void cbca(float *x0, float *x1, float *vol, float *vol_out, int size, int size2, int size3, int L1, int L2, float tau1, float tau2)
+{
+    int output_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (output_id < size) {
+		int d = output_id;
+        const int x = d % size3;
+        d /= size3;
+        const int y = d % size2;
+        d /= size2;
+
+        int yn, ys;
+        for (yn = y - 1; CBCA_CONDITIONS(x, yn, x, yn + 1); yn--) {};
+        for (ys = y + 1; CBCA_CONDITIONS(x, ys, x, ys - 1); ys++) {};
+
+        float sum = 0;
+        int cnt = 0;
+
+        /* output */
+        for (int yy = yn + 1; yy < ys; yy++) {
+            int xe, xw;
+            for (xe = x - 1; CBCA_CONDITIONS(xe, yy, xe + 1, yy); xe--) {};
+            for (xw = x + 1; CBCA_CONDITIONS(xw, yy, xw - 1, yy); xw++) {};
+
+            for (int xx = xe + 1; xx < xw; xx++) {
+				sum += vol[(d * size2 + yy) * size3 + xx];
+				cnt++;
+            }
+        }
+		vol_out[output_id] = sum / cnt;
+	}
+}
+
+/* cross-based cost aggregation */
+int cbca(lua_State *L)
+{
+    THCudaTensor *x0 = (THCudaTensor*)luaT_checkudata(L, 1, "torch.CudaTensor");
+    THCudaTensor *x1 = (THCudaTensor*)luaT_checkudata(L, 2, "torch.CudaTensor");
+    THCudaTensor *vol = (THCudaTensor*)luaT_checkudata(L, 3, "torch.CudaTensor");
+    THCudaTensor *vol_out = (THCudaTensor*)luaT_checkudata(L, 4, "torch.CudaTensor");
+    int L1 = luaL_checkinteger(L, 5);
+    int L2 = luaL_checkinteger(L, 6);
+    float tau1 = luaL_checknumber(L, 7);
+    float tau2 = luaL_checknumber(L, 8);
+
+    cbca<<<(THCudaTensor_nElement(vol) - 1) / TB + 1, TB>>>(
+        THCudaTensor_data(x0),
+        THCudaTensor_data(x1),
+        THCudaTensor_data(vol),
+        THCudaTensor_data(vol_out),
+        THCudaTensor_nElement(vol),
+        THCudaTensor_size(x0, 2),
+        THCudaTensor_size(x0, 3),
+        L1, L2, tau1, tau2);
+    checkCudaError(L);
+    return 0;
+}
+
 
 static const struct luaL_Reg funcs[] = {
 	{"ad", ad},
 	{"census", census},
+	{"cbca", cbca},
 	{"spatial_argmin", spatial_argmin},
 	{NULL, NULL}
 };
--- main.lua
+++ main.lua
@@ -5,47 +5,51 @@ require 'libadcensus'
 
 height = 288
 width = 384
-disp_max = 16
+disp_max = 18
 ad_lambda = 10
 census_lambda = 30
+L1 = 34
+L2 = 17
+tau1 = 20
+tau2 = 6
 
 cutorch.setDevice(1)
-
-function ad(x0, x1)
-   local vol = torch.CudaTensor(1, disp_max, height, width)
-   adcensus.ad(x0, x1, vol)
-   return vol
-end
-
-function census(x0, x1)
-   local vol = torch.CudaTensor(1, disp_max, height, width)
-   adcensus.census(x0, x1, vol)
-   return vol
-end
-
-
-function combine_cost(ad_vol, ad_lambda, census_vol, census_lambda)
-   -- modifies c
-   function rho(c, lambda)
-      c:mul(-1 / lambda):exp():mul(-1):add(1)
-   end
-
-   rho(ad_vol, ad_lambda)
-   rho(census_vol, census_lambda)
-
-   local vol = torch.CudaTensor(1, disp_max, height, width)
-   return vol:add(ad_vol, 1, census_vol)
-end
 
 x0 = image.loadPNG('data/tsukuba0.png', 3, 'byte'):float():resize(1, 3, height, width):cuda()
 x1 = image.loadPNG('data/tsukuba1.png', 3, 'byte'):float():resize(1, 3, height, width):cuda()
 pred = torch.CudaTensor(1, 1, height, width)
 
-ad_vol = ad(x0, x1)
-census_vol = census(x0, x1)
+-- ad volume
+ad_vol = torch.CudaTensor(1, disp_max, height, width)
+adcensus.ad(x0, x1, ad_vol)
 
-adcensus_vol = combine_cost(ad_vol, ad_lambda, census_vol, census_lambda)
+-- census volume
+census_vol = torch.CudaTensor(1, disp_max, height, width)
+adcensus.census(x0, x1, census_vol)
 
+-- adcensus volume
+function rho(c, lambda)
+   c:mul(-1 / lambda):exp():mul(-1):add(1)
+end
+rho(ad_vol, ad_lambda)
+rho(census_vol, census_lambda)
+adcensus_vol = torch.CudaTensor(1, disp_max, height, width)
+adcensus_vol:add(ad_vol, 1, census_vol)
+
+-- cbca
+x0_t = torch.CudaTensor(1, 3, width, height):copy(x0:transpose(3, 4))
+x1_t = torch.CudaTensor(1, 3, width, height):copy(x1:transpose(3, 4))
+input = adcensus_vol
+input_t = torch.CudaTensor(1, disp_max, width, height)
+output_t = torch.CudaTensor(1, disp_max, width, height)
+output = torch.CudaTensor(1, disp_max, height, width)
+
+for i = 1,4 do
+   adcensus.cbca(x0, x1, input, output, L1, L2, tau1, tau2)
+   input:copy(output)
+end
+
+-- plot
 adcensus.spatial_argmin(adcensus_vol, pred)
 pred:add(-1):div(disp_max)
-image.savePNG('foo.adcensus.png', pred[{1,1}])
+image.savePNG('foo.png', pred[{1,1}])
